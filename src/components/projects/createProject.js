6783f79755430f7238ab3c21adf44a8d63f0e1e2
fadeomar <fadeomar2015@gmail.com>
2019-12-16 23:40:30 +0200

    add bg and edit style

--- src/components/projects/createProject.js
+++ src/components/projects/createProject.js
@@ -1,1 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class CreateProject extends Component {
+  state = {
+    title: '',
+    content: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Create a Project</h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange} value={this.state.title} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">Project Content</label>
+            <textarea
+              className="materialize-textarea"
+              name="content"
+              id="content"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="input-filed">
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateProject;

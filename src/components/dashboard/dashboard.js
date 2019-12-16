7844d77d2d2b6e29bafccfd9fd5dac9ca08b79c4
fadeomar <fadeomar2015@gmail.com>
2019-12-16 20:11:35 +0200

    crate the Notifications components

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,1 +1,21 @@
 import React from 'react';
+import Notifications from './notification';
+
+class Dashboard extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div className="dashboard container">
+        <div className="row">
+          <div className="co1 s12 m6" />
+          <div className="co1 s12 m5 offest-m1">
+            <Notifications />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
--- src/components/dashboard/notification.js
+++ src/components/dashboard/notification.js
@@ -1,1 +1,11 @@
 import React from 'react';
+
+const Notifications = () => {
+  return (
+    <div>
+      <p>Notifications</p>
+    </div>
+  );
+};
+
+export default Notifications;

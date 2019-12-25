0cd9e28febb75456597aedc154f3899e5da7ca2a
fadeomar <fadeomar2015@gmail.com>
2019-12-25 13:27:08 +0200

    link the project by id

--- src/components/projects/projectList.js
+++ src/components/projects/projectList.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ProjectSummary from './projectSummary';
 
 const ProjectList = ({ projects }) => {
-  console.log(projects);
   return (
     <div className="list section">
-      {projects && projects.map(project => <ProjectSummary project={project} key={project.id} />)}
+      {projects &&
+        projects.map(project => {
+          console.log('idddd', project.id);
+
+          return (
+            <Link to={`/project/${project.id}`}>
+              <ProjectSummary project={project} key={project.id} />
+            </Link>
+          );
+        })}
     </div>
   );
 };

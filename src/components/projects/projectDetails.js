afa2d170aad6e4498f36321b71c4c4671843e85a
fadeomar <fadeomar2015@gmail.com>
2019-12-28 00:13:18 +0200

    complete the projectDetails component and add auth

--- src/components/projects/projectDetails.js
+++ src/components/projects/projectDetails.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { firebaseConnect, firestoreConnect } from 'react-redux-firebase';
+import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import { Redirect } from 'react-router-dom';
 
 const ProjectDetails = props => {
-  const { id } = props.match.params;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Project title</span>
-          <p>asdasdasdasdasdasd</p>
-        </div>
-        <div className="card-action gret lighten-4 grey-text">
-          <div>Posted by fadi </div>
-          <div>2nd september, 2am</div>
+  const { auth, project } = props;
+  console.log(props);
+  if (!auth.uid) {
+    return <Redirect to="/signin" />;
+  }
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action gret lighten-4 grey-text">
+            <div>
+              Posted by {project.authorFirstName} {project.authorLastName}
+            </div>
+            <div>2nd september, 2am</div>
+          </div>
         </div>
       </div>
+    );
+  }
+  return (
+    <div className="container center">
+      <p>Loading project...</p>
     </div>
   );
 };
@@ -27,6 +41,7 @@ const mapStateToProps = (state, prevProps) => {
   const project = projects ? projects[id] : null;
   return {
     project,
+    auth: state.firebase.auth,
   };
 };
 

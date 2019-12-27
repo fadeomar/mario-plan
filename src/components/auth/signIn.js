b765c3b27b74e2c68327f3cab20b857a9454fbba
fadeomar <fadeomar2015@gmail.com>
2019-12-28 00:16:59 +0200

    add auth to signin component

--- src/components/auth/signIn.js
+++ src/components/auth/signIn.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { signIn } from '../../store/actions/authActions';
 
 class SignIn extends Component {
@@ -18,7 +19,10 @@ class SignIn extends Component {
   };
 
   render() {
-    const { authError } = this.props;
+    const { authError, auth } = this.props;
+    if (auth.uid) {
+      return <Redirect to="/" />;
+    }
     return (
       <div className="container">
         <form className="white" onSubmit={this.handleSubmit}>
@@ -49,6 +53,7 @@ class SignIn extends Component {
 const mapStateToProps = state => {
   return {
     authError: state.auth.authError,
+    auth: state.firebase.auth,
   };
 };
 

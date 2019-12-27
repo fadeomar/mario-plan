d359fbeada4ec6706ba63aae8be5360602f9c09f
fadeomar <fadeomar2015@gmail.com>
2019-12-27 23:39:40 +0200

    links depend on auth status

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -4,23 +4,25 @@ import { connect } from 'react-redux';
 import SignedInLinks from './signedInLinks';
 import SignedOutLinks from './signedOutLinks';
 
-const Navbar = () => {
+const Navbar = props => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           Mario Plan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log('navbar', state);
-  return {};
+  return {
+    auth: state.firebase.auth,
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);

9c29b515ef600d20b1a268fc77f4063fe1a2d8c4
fadeomar <fadeomar2015@gmail.com>
2019-12-28 16:34:17 +0200

    loading profile in the firebase and use it in create project component

--- src/store/actions/projectAction.js
+++ src/store/actions/projectAction.js
@@ -1,18 +1,19 @@
 const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     const fireStore = getFirestore();
+    const {profile} = getState().firebase;
+    const userId = getState().firebase.auth.uid;
     fireStore
       .collection('projects')
       .add({
         ...project,
-        authorFirstName: 'fadi',
-        authorLastName: 'omar',
-        authorId: '132',
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: userId,
         createAt: new Date(),
       })
       .then(() => dispatch({ type: 'CREATE_PROJECT', project }))
       .catch(e => {
-        console.log('error aaa', e);
         dispatch({ type: 'CREATE_PROJECT_ERROR', e });
       });
   };

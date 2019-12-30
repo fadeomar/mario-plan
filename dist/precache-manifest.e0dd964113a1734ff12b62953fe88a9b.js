ecc87a1ce508081e3e262784b72d3025666fc0f2
fadeomar <fadeomar2015@gmail.com>
2019-12-30 14:32:31 +0200

    build the react app and move it to the dest folder

---
+++ dist/precache-manifest.e0dd964113a1734ff12b62953fe88a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c17e7803214c6216fb5cab5c554cde1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "232f6e17a67ee7215235",
+    "url": "/static/css/main.21463ae2.chunk.css"
+  },
+  {
+    "revision": "7c15a4ea09a2d4b20403",
+    "url": "/static/js/2.a8dfccef.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/static/js/2.a8dfccef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "232f6e17a67ee7215235",
+    "url": "/static/js/main.edd5d6a3.chunk.js"
+  },
+  {
+    "revision": "3e75b7e79fb453e22747",
+    "url": "/static/js/runtime-main.634f6d3c.js"
+  }
+]);
